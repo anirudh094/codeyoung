0b6f100ea78259cb8e30ffb148cc4ba7ba9fb14e
anirudh094 <anirudhgarg094@gmail.com>
2024-03-11 20:17:51 +0530

    Firebase added

--- src/Pages/FreeTrial.js
+++ src/Pages/FreeTrial.js
@@ -1,7 +1,80 @@
 import React from "react";
 import "../CSS/FreeTrial.css";
+import { useState } from "react";
 
 function FreeTrial() {
+  const [user, setUser] = useState({
+    parentName: "",
+    parentPhoneNumber: "",
+    parentEmail: "",
+    course: "",
+    childGrade: "",
+    coupon: "",
+  });
+
+  let name, value;
+  const getUserData = (event) => {
+    name = event.target.name;
+    value = event.target.value;
+
+    setUser({ ...user, [name]: value });
+  };
+
+  const postData = async (e) => {
+    e.preventDefault();
+
+    const {
+      parentName,
+      parentPhoneNumber,
+      parentEmail,
+      course,
+      childGrade,
+      coupon,
+    } = user;
+
+    if (
+      parentName &&
+      parentPhoneNumber &&
+      parentEmail &&
+      course &&
+      childGrade &&
+      coupon
+    ) {
+      const res = await fetch(
+        "https://codeyoung-d76a3-default-rtdb.firebaseio.com/freetrial.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            parentName,
+            parentPhoneNumber,
+            parentEmail,
+            course,
+            childGrade,
+            coupon,
+          }),
+        }
+      );
+
+      if (res) {
+        setUser({
+          parentName: "",
+          parentPhoneNumber: "",
+          parentEmail: "",
+          course: "",
+          childGrade: "",
+          coupon: "",
+        });
+
+        alert("Data stored successfully");
+      }
+    } else {
+      alert("Fill all data");
+    }
+  };
+
   return (
     <>
       <div className="free-trial-body">
@@ -30,58 +103,198 @@ function FreeTrial() {
             </div>
             <form>
               <p>Parent Name</p>
-              <input type="text" placeholder="Name here"></input>
+              <input
+                name="parentName"
+                type="text"
+                placeholder="Name here"
+                value={user.parentName}
+                onChange={getUserData}
+                required
+              ></input>
 
               <p>Parent Phone Number</p>
-              <input type="number" placeholder="Phone Number"></input>
+              <input
+                name="parentPhoneNumber"
+                type="number"
+                placeholder="Phone Number"
+                value={user.parentPhoneNumber}
+                onChange={getUserData}
+                required
+              ></input>
 
               <p>Parent Email Address</p>
-              <input type="email" placeholder="Email ID here"></input>
+              <input
+                name="parentEmail"
+                type="email"
+                placeholder="Email ID here"
+                value={user.parentEmail}
+                onChange={getUserData}
+                required
+              ></input>
 
               <p>Course preference</p>
               <div className="form-course-preference">
-                <input type="checkbox" id="coding" />
+                <input
+                  name="course"
+                  type="checkbox"
+                  id="coding"
+                  value="coding"
+                  onChange={getUserData}
+                  required
+                />
                 <label for="coding">Coding</label>
-                <input type="checkbox" id="Maths" />
+                <input
+                  name="course"
+                  type="checkbox"
+                  id="Maths"
+                  value="Maths"
+                  onChange={getUserData}
+                  required
+                />
                 <label for="Maths">Maths</label>
-                <input type="checkbox" id="robotics" />
+                <input
+                  name="course"
+                  type="checkbox"
+                  id="robotics"
+                  value="robotics"
+                  onChange={getUserData}
+                  required
+                />
                 <label for="robotics">Robotics</label>
-                <input type="checkbox" id="science" />
+                <input
+                  name="course"
+                  type="checkbox"
+                  id="science"
+                  value="science"
+                  onChange={getUserData}
+                  required
+                />
                 <label for="science">Science</label>
               </div>
 
               <p>Childs Grade</p>
               <div className="form-child-grade">
-                <input type="radio" id="1st" name="Child_Grade" value="1st" />
+                <input
+                  type="radio"
+                  id="1st"
+                  name="childGrade"
+                  value="1st"
+                  onChange={getUserData}
+                  required
+                />
                 <label for="1st">1st</label>
-                <input type="radio" id="2nd" name="Child_Grade" value="2nd" />
+                <input
+                  type="radio"
+                  id="2nd"
+                  name="childGrade"
+                  value="2nd"
+                  onChange={getUserData}
+                  required
+                />
                 <label for="2nd">2nd</label>
-                <input type="radio" id="3rd" name="Child_Grade" value="3rd" />
+                <input
+                  type="radio"
+                  id="3rd"
+                  name="childGrade"
+                  value="3rd"
+                  onChange={getUserData}
+                  required
+                />
                 <label for="3rd">3rd</label>
-                <input type="radio" id="4th" name="Child_Grade" value="4th" />
+                <input
+                  type="radio"
+                  id="4th"
+                  name="childGrade"
+                  value="4th"
+                  onChange={getUserData}
+                  required
+                />
                 <label for="4th">4th</label>
-                <input type="radio" id="5th" name="Child_Grade" value="5th" />
+                <input
+                  type="radio"
+                  id="5th"
+                  name="childGrade"
+                  value="5th"
+                  onChange={getUserData}
+                  required
+                />
                 <label for="5th">5th</label>
-                <input type="radio" id="6th" name="Child_Grade" value="6th" />
+                <input
+                  type="radio"
+                  id="6th"
+                  name="childGrade"
+                  value="6th"
+                  onChange={getUserData}
+                  required
+                />
                 <label for="6th">6th</label>
               </div>
               <div className="form-child-grade">
-                <input type="radio" id="7th" name="Child_Grade" value="7th" />
+                <input
+                  type="radio"
+                  id="7th"
+                  name="childGrade"
+                  value="7th"
+                  onChange={getUserData}
+                  required
+                />
                 <label for="7th">7th</label>
-                <input type="radio" id="8th" name="Child_Grade" value="8th" />
+                <input
+                  type="radio"
+                  id="8th"
+                  name="childGrade"
+                  value="8th"
+                  onChange={getUserData}
+                  required
+                />
                 <label for="8th">8th</label>
-                <input type="radio" id="9th" name="Child_Grade" value="9th" />
+                <input
+                  type="radio"
+                  id="9th"
+                  name="childGrade"
+                  value="9th"
+                  onChange={getUserData}
+                  required
+                />
                 <label for="9th">9th</label>
-                <input type="radio" id="10th" name="Child_Grade" value="10th" />
+                <input
+                  type="radio"
+                  id="10th"
+                  name="childGrade"
+                  value="10th"
+                  onChange={getUserData}
+                  required
+                />
                 <label for="10th">10th</label>
-                <input type="radio" id="11th" name="Child_Grade" value="11th" />
+                <input
+                  type="radio"
+                  id="11th"
+                  name="childGrade"
+                  value="11th"
+                  onChange={getUserData}
+                  required
+                />
                 <label for="11th">11th</label>
-                <input type="radio" id="12th" name="Child_Grade" value="12th" />
+                <input
+                  type="radio"
+                  id="12th"
+                  name="childGrade"
+                  value="12th"
+                  onChange={getUserData}
+                  required
+                />
                 <label for="12th">12th</label>
               </div>
 
               <p>Coupon Code</p>
-              <input type="text" placeholder="Coupon Code"></input>
+              <input
+                name="coupon"
+                type="text"
+                placeholder="Coupon Code"
+                value={user.coupon}
+                onChange={getUserData}
+                required
+              ></input>
               <br></br>
 
               <div className="form-tnc">
@@ -100,7 +313,13 @@ function FreeTrial() {
                 </label>
               </div>
 
-              <button className="time-slot-btn">Book Free Trial</button>
+              <button
+                className="time-slot-btn"
+                type="submit"
+                onClick={postData}
+              >
+                Book Free Trial
+              </button>
             </form>
           </div>
         </div>
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,13 +1,36 @@
-import React from 'react'
-import "../CSS/Login.css"
+import React from "react";
+import "../CSS/Login.scss";
 function Login() {
   return (
-    <div className='login-body'>
-        <div className='login-body-header'>
-        Welcome to Student Portal
-        </div>
+    <div className="login-body">
+      <div className="login-body-header">Welcome to Student Portal</div>
+      <div class="form__group field">
+        <input
+          type="input"
+          className="form__field"
+          placeholder="Name"
+          name="name"
+          id="name"
+          required
+        />
+        <label for="name" className="form__label">
+          Please enter Student Email/Username
+        </label>
+      </div>
+      <button className="login-body-btn">Continue</button>
+      <div className="login-body-content">
+        Please use Registered Email/Username given in Codeyoung to login
+      </div>
+      <img
+        className="login-page-logo"
+        src="https://www.codeyoung.com/_nuxt/img/codeyoung-logo.4c8144c.svg"
+        alt="logo"
+      ></img>
+      <div className="login-body-content-2">
+        Login to access a community of 10000+ members and thousands of resources
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
